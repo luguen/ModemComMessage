aa30ec09f29a3c6d9687228e9a477bb0e04b8574
luguen <bertrand.Leroy@technicolor.com>
2017-01-12 15:53:11 +0100

    Notification icon Stop the process after the execution of the plugin Use Multibyte instead of Unicode

--- ModemComMessage/ModemComMessage.cpp
+++ ModemComMessage/ModemComMessage.cpp
@@ -20,23 +20,15 @@ void CModemComMessage::getName(char* szName)
 void CModemComMessage::init(const char* szPluginsDir)
 {
 }
-/*
-const wchar_t *GetWC(const char *c)
-{
-	const size_t cSize = strlen(c) + 1;
-	wchar_t* wc = new wchar_t[cSize];
-	mbstowcs(wc, c, cSize);
 
-	return wc;
-}
-*/
 bool CModemComMessage::putData(char* szDate, char* szTime, char* szNumber, char* szName)
 {
 	char _szMsg[256];
 	sprintf(_szMsg, "[Le:%s]\n[A:%s]\n[Num:%s]\n[Nom:%s]", szDate, szTime, szNumber, szName);
-	MessageBox(NULL, _szMsg, "Appel !", MB_ICONEXCLAMATION | MB_OK);
+	MessageBox(NULL, _szMsg, "Appel !", MB_ICONINFORMATION | MB_OK);
 
-	return false;
+	// Stop the process now
+	return true;
 }
 
 void* CModemComMessage::getPropertyPage()
